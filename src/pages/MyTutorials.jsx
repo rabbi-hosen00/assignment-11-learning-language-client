a26ca5ada1f19d01451bbbe8d1c1131d14c71394
rabbi hosen <rabbihosen01581@gmail.com>
2025-02-12 19:18:59 +0600

    navbar fixed

--- src/pages/MyTutorials.jsx
+++ src/pages/MyTutorials.jsx
@@ -363,7 +363,7 @@ const MyTutorials = () => {
   };
 
   return (
-    <div className="max-w-5xl mx-auto p-4 mt-16">
+    <div className="container mx-auto p-4 mt-16">
       <div className="text-center mb-6">
         <h1 className="text-3xl font-bold text-orange-500">My Tutorials</h1>
       </div>
